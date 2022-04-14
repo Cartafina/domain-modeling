f8cccf073e621e71d6404e03fa2ef793bbcd3a81
Cartafina <camila.cartafina@kellogg.northwestern.edu>
2022-04-14 02:44:34 +0000

    finish crm and tacostagram

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,37 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
-DROP TABLE IF EXISTS 
+DROP TABLE IF EXISTS salespeople;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
 -- CREATE TABLES
+CREATE TABLE salespeople (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+);
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    adress TEXT,
+    phone TEXT
+    salesperson_id INTEGER,
+    company_id INTEGER
+);
+CREATE TABLE activities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    date TEXT,
+    time TEXT,
+    notes TEXT,
+    contact_id INTEGER
+    salesperson_id INTEGER
+);
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,33 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS followers;
 
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    location TEXT
+);
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time TEXT,
+    user_id INTEGER
+);
+CREATE TABLE likes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    photo_id INTEGER
+    user_id INTEGER
+);
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    photo_id INTEGER
+    note TEXT,
+    user_id INTEGER
+);
+CREATE TABLE followers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER
+    follower_id INTEGER
+);
